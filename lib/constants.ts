08392d064f176df4b7583bb6d9ea355d7383d62e
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-05-06 17:16:45 +0530

    02-create website layout and header and footer

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_NAME || 'NexusStore'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Embrace the slow, enrich the soul'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  'An online store that offers a curated selection of slow fashion, sustainable products, and artisanal goods. We believe in the power of mindful consumption and aim to provide a platform for conscious shoppers who value quality over quantity.'
---
+++ lib/data.ts
@@ -0,0 +1,29 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deals",
+      href: '/search?tag=todays-deal',
+    },
+    {
+      name: 'Best Sellers',
+      href: '/search?tag=best-seller',
+    },
+    {
+      name: 'Featured Products',
+      href: '/search?tag=featured',
+    },
+    {
+      name: 'New Arrivals',
+      href: '/search?tag=new-arrival',
+    },
+    {
+      name: 'Browing History',
+      href: '/#browsing-history',
+    },
+    {
+      name: 'Customer Service',
+      href: '/page/customer-service',
+    },
+  ],
+}
+export default data
