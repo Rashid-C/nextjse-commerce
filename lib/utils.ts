ee6b5c80dea2d82e0e9a3b3e012053c53d96e418
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-05-08 00:55:17 +0530

    04-connect-to-mongodbAtlas-and- seed-products

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,20 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatNumberWithDecimal = (num: number): string => {
+  const [int, decimal] = num.toString().split('.')
+  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : int
+}
+
+//prompt:gpt=create toSlug ts arrow funtion that convert text to lowercase, remove non-word, non-whitespace,non-hyphen characters, replace whitespace,trim leading hyphens and trim trailing hyphens
+
+export const toSlug = (text: string): string =>
+  text
+    .toLowerCase()
+    .replace(/[^\w\s-]/g, '') // Remove non-word, non-whitespace, non-hyphen chars
+    .replace(/\s+/g, '-') // Replace whitespace with hyphens
+    .replace(/^-+|-+$/g, '') // Trim leading/trailing hyphens
